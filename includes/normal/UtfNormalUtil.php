jkluk/mediawiki-trunk-phase3

commit bbf3e03e418ca50411585e3b54aca5d81a414051

The beginnings of HipHop compiled mode support. It works now for parser cache hits.
* Work around HipHop issue 314 (volatile broken) and issue 308 (no compilation detection) by adding some large and ugly compilation detection code to WebStart.php and doMaintenance.php.
* Provide an MW_COMPILED constant which can be used to detect compiled mode throughout the codebase.
* Introduced wfIsHipHop(), which detects either compiled or interpreted mode. Used this to work around unusual eval() return value in eval.php.
* Work around lack of ini_get() in Maintenance.php, by duplicating wfIsHipHop(). 
* In Maintenance::shouldExecute(), accept "include" as an inclusion function name, since all kinds of inclusion give this string in HipHop.
* Introduced new class MWInit, which provides some static functions in the pre-autoloader environment.
* Introduced MWInit::compiledPath(), which provides a relative path for invoking a compiled file, and MWInit::interpretedPath(), which provides an absolute path for interpreting a PHP file. Used these new functions in the appropriate places.
* When we are running compiled code, don't include files which would generate duplicate class, function or constant definitions. Documented the new requirements on the contents of Defines.php and UtfNormalDefines.php.
* In HipHop compiled mode, it's not possible to have executable code in the same file as a class definition. 
  ** Moved MimeMagic initialisation to the constructor.
  ** Moved Namespace.php global variable initialisation to Setup.php.
  ** Moved MemcachedSessions.php initialisation to the caller in GlobalFunctions.php.
  ** Moved Sanitizer.php constants and global variables to static class members. Introduced an accessor function for the attribs regex, as a new place to put code formerly at file level. 
  ** Moved Language.php initialisation of $wgLanguageNames to Language::getLanguageNames(). Removed the global variable, marked "private" since forever.

includes/normal/UtfNormalUtil.php
@@ -25,7 +25,7 @@
  * @ingroup UtfNormal
  */
 
-require_once dirname(__FILE__).'/UtfNormalDefines.php';
+require_once( MWInit::compiledPath( 'includes/normal/UtfNormalDefines.php' ) );
 
 /**
  * Return UTF-8 sequence for a given Unicode code point.

includes/AutoLoader.php
@@ -457,6 +457,22 @@ $wgAutoloadLocalClasses = array(
 	'LBFactory_InstallerFake' => 'includes/installer/Installer.php',
 	'LocalSettingsGenerator' => 'includes/installer/LocalSettingsGenerator.php',
 	'WebInstaller' => 'includes/installer/WebInstaller.php',
+	'WebInstaller_Complete' => 'includes/installer/WebInstallerPage.php',
+	'WebInstaller_Copying' => 'includes/installer/WebInstallerPage.php',
+	'WebInstaller_DBConnect' => 'includes/installer/WebInstallerPage.php',
+	'WebInstaller_DBSettings' => 'includes/installer/WebInstallerPage.php',
+	'WebInstaller_Document' => 'includes/installer/WebInstallerPage.php',
+	'WebInstaller_ExistingWiki' => 'includes/installer/WebInstallerPage.php',
+	'WebInstaller_Install' => 'includes/installer/WebInstallerPage.php',
+	'WebInstaller_Language' => 'includes/installer/WebInstallerPage.php',
+	'WebInstaller_Name' => 'includes/installer/WebInstallerPage.php',
+	'WebInstaller_Options' => 'includes/installer/WebInstallerPage.php',
+	'WebInstaller_Readme' => 'includes/installer/WebInstallerPage.php',
+	'WebInstaller_ReleaseNotes' => 'includes/installer/WebInstallerPage.php',
+	'WebInstaller_Restart' => 'includes/installer/WebInstallerPage.php',
+	'WebInstaller_Upgrade' => 'includes/installer/WebInstallerPage.php',
+	'WebInstaller_UpgradeDoc' => 'includes/installer/WebInstallerPage.php',
+	'WebInstaller_Welcome' => 'includes/installer/WebInstallerPage.php',
 	'WebInstallerPage' => 'includes/installer/WebInstallerPage.php',
 	'WebInstallerOutput' => 'includes/installer/WebInstallerOutput.php',
 	'MysqlInstaller' => 'includes/installer/MysqlInstaller.php',

includes/DefaultSettings.php
@@ -28,7 +28,9 @@ if( !defined( 'MEDIAWIKI' ) ) {
 
 # Create a site configuration object. Not used for much in a default install
 if ( !defined( 'MW_PHP4' ) ) {
-	require_once( "$IP/includes/SiteConfiguration.php" );
+	if ( !defined( 'MW_COMPILED' ) ) {
+		require_once( "$IP/includes/SiteConfiguration.php" );
+	}
 	$wgConf = new SiteConfiguration;
 }
 /** @endcond */

includes/Defines.php
@@ -1,6 +1,11 @@
 <?php
 /**
- * A few constants that might be needed during LocalSettings.php
+ * A few constants that might be needed during LocalSettings.php.
+ *
+ * Note: these constants must all be resolvable at compile time by HipHop, 
+ * since this file will not be executed during request startup for a compiled
+ * MediaWiki.
+ *
  * @file
  */
 

includes/ExternalStore.php
@@ -67,7 +67,7 @@ class ExternalStore {
 
 		$class = 'ExternalStore' . ucfirst( $proto );
 		/* Any custom modules should be added to $wgAutoLoadClasses for on-demand loading */
-		if( !class_exists( $class ) ) {
+		if( !MWInit::classExists( $class ) ) {
 			return false;
 		}
 

includes/GlobalFunctions.php
@@ -8,7 +8,9 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( "This file is part of MediaWiki, it is not a valid entry point" );
 }
 
-require_once dirname( __FILE__ ) . '/normal/UtfNormalUtil.php';
+if ( !defined( 'MW_COMPILED' ) ) {
+	require_once( dirname( __FILE__ ) . '/normal/UtfNormalUtil.php' );
+}
 
 // Hide compatibility functions from Doxygen
 /// @cond
@@ -329,7 +331,7 @@ function wfErrorLog( $text, $file ) {
 		$exists = file_exists( $file );
 		$size = $exists ? filesize( $file ) : false;
 		if ( !$exists || ( $size !== false && $size + strlen( $text ) < 0x7fffffff ) ) {
-			error_log( $text, 3, $file );
+			file_put_contents( $file, $text, FILE_APPEND );
 		}
 		wfRestoreWarnings();
 	}
@@ -495,7 +497,7 @@ function wfMessage( $key /*...*/) {
  */
 function wfMessageFallback( /*...*/ ) {
 	$args = func_get_args();
-	return call_user_func_array( array( 'Message', 'newFallbackSequence' ), $args );
+	return MWFunction::callArray( 'Message::newFallbackSequence', $args );
 }
 
 /**
@@ -1994,6 +1996,13 @@ function wfIsWindows() {
 }
 
 /**
+ * Check if we are running under HipHop
+ */
+function wfIsHipHop() {
+	return function_exists( 'hphp_thread_set_warmup_enabled' );
+}
+
+/**
  * Swap two variables
  */
 function swap( &$x, &$y ) {
@@ -2781,7 +2790,17 @@ function wfSetupSession( $sessionId = false ) {
 	global $wgSessionsInMemcached, $wgCookiePath, $wgCookieDomain,
 			$wgCookieSecure, $wgCookieHttpOnly, $wgSessionHandler;
 	if( $wgSessionsInMemcached ) {
-		require_once( 'MemcachedSessions.php' );
+		if ( !defined( 'MW_COMPILED' ) ) {
+			require_once( 'MemcachedSessions.php' );
+		}
+		session_set_save_handler( 'memsess_open', 'memsess_close', 'memsess_read', 
+			'memsess_write', 'memsess_destroy', 'memsess_gc' );
+
+		// It's necessary to register a shutdown function to call session_write_close(), 
+		// because by the time the request shutdown function for the session module is 
+		// called, $wgMemc has already been destroyed. Shutdown functions registered
+		// this way are called before object destruction.
+		register_shutdown_function( 'memsess_write_close' );
 	} elseif( $wgSessionHandler && $wgSessionHandler != ini_get( 'session.save_handler' ) ) {
 		# Only set this if $wgSessionHandler isn't null and session.save_handler
 		# hasn't already been set to the desired value (that causes errors)

includes/MemcachedSessions.php
@@ -96,10 +96,3 @@ function memsess_write_close() {
 	session_write_close();
 }
 
-session_set_save_handler( 'memsess_open', 'memsess_close', 'memsess_read', 'memsess_write', 'memsess_destroy', 'memsess_gc' );
-
-// It's necessary to register a shutdown function to call session_write_close(), 
-// because by the time the request shutdown function for the session module is 
-// called, $wgMemc has already been destroyed. Shutdown functions registered
-// this way are called before object destruction.
-register_shutdown_function( 'memsess_write_close' );

includes/MimeMagic.php
@@ -117,14 +117,6 @@ unknown/unknown application/octet-stream application/x-empty [UNKNOWN]
 END_STRING
 );
 
-// Note: because this file is possibly included by a function,
-// we need to access the global scope explicitely!
-global $wgLoadFileinfoExtension;
-
-if ( $wgLoadFileinfoExtension ) {
-	wfDl( 'fileinfo' );
-}
-
 /**
  * Implements functions related to mime types such as detection and mapping to
  * file extension.
@@ -160,6 +152,10 @@ class MimeMagic {
 	 */
 	private static $instance;
 
+	/** True if the fileinfo extension has been loaded
+	 */
+	private static $extensionLoaded = false;
+
 	/** Initializes the MimeMagic object. This is called by MimeMagic::singleton().
 	*
 	* This constructor parses the mime.types and mime.info files and build internal mappings.
@@ -169,12 +165,17 @@ class MimeMagic {
 		*   --- load mime.types ---
 		*/
 
-		global $wgMimeTypeFile, $IP;
+		global $wgMimeTypeFile, $IP, $wgLoadFileinfoExtension;
 
 		$types = MM_WELL_KNOWN_MIME_TYPES;
 
 		if ( $wgMimeTypeFile == 'includes/mime.types' ) {
 			$wgMimeTypeFile = "$IP/$wgMimeTypeFile";
+		}
+
+		if ( $wgLoadFileinfoExtension && !self::$extensionLoaded ) {
+			self::$extensionLoaded = true;
+			wfDl( 'fileinfo' );
 		}
 
 		if ( $wgMimeTypeFile ) {

commit 0f3e19f25d9eb5bc8f7020d2cb858b1292e94759

(bug 28864) Fix UtfNormal benchmark & test case runners from regression in r85327, r85944
It's possible that this has some side effect on HipHop builds -- the require_once on UtfNormalDefines.php that had been moved from UtfNormalUtil.php to MediaWiki's Setup.php in r85944 had been previously damaged by r85327 sticking a MediaWiki-specific MWInit class static method call into it, making it break on all the generator & test & benchmark code which does not depend on MediaWiki.
With the require_once restored everything seems to work, though it's possible that it ends up trying to include twice on HipHop -- have not tested.





git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@87748 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/normal/UtfNormalUtil.php
@@ -25,6 +25,9 @@
  * @ingroup UtfNormal
  */
 
+/** */
+require_once dirname(__FILE__).'/UtfNormalDefines.php';
+
 /**
  * Return UTF-8 sequence for a given Unicode code point.
  * May die if fed out of range data.